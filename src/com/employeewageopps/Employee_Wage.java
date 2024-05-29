ac0560ccb2438f5a27156246748abc0477ccde48
smitalpatil98 <patilsmital.1101998gmail.com>
2024-05-29 12:21:25 +0530

    UC_10

--- src/com/employeewageopps/Employee_Wage.java
+++ src/com/employeewageopps/Employee_Wage.java
@@ -1,19 +1,14 @@
-//Ability to save the Total Wage for Each Company -
-// Note: You can Create EmpWageBuilder for each Company - Use Instance Variable instead of function parameters
 package com.employeewageopps;
 
-public class Employee_Wage {
-    int FULL_TIME_EMP = 1;
-    int PART_TIME_EMP = 2;
-    int EMP_FULLTIME_WORK_HOUR = 8;
-    int EMP_PARTTIME_WORK_HOUR = 4;
-    int total_wage = 0;
+class CompanyEmpWage{
+    //commit
     String company;
     int EMP_RATE_PER_HOUR;
     int EMP_WORK_DAYS;
     int EMP_WORK_HOUR;
+    int total_wage;
 
-    public Employee_Wage(String company, int EMP_RATE_PER_HOUR, int EMP_WORK_DAYS, int EMP_WORK_HOUR) {
+    public CompanyEmpWage(String company,int EMP_RATE_PER_HOUR,int EMP_WORK_DAYS,int EMP_WORK_HOUR){
 
         this.company = company;
         this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
@@ -21,46 +16,84 @@ public class Employee_Wage {
         this.EMP_WORK_HOUR = EMP_WORK_HOUR;
     }
 
+    public void setTotalWage(int total_wage){
+        this.total_wage = total_wage;
+    }
 
-    public int calculateEmployeeWage() {
+    @Override
+    public String toString(){
+        return "Total Employee Wage for company " +company + " is : " + total_wage;
+    }
+
+}
+
+public class Employee_Wage {
+    int FULL_TIME_EMP = 1;
+    int PART_TIME_EMP = 2;
+    int EMP_FULLTIME_WORK_HOUR = 8;
+    int EMP_PARTTIME_WORK_HOUR = 4;
+
+
+    int noOfCompany = 0;
+    CompanyEmpWage[] companyEmpWageArray ;
+
+    public Employee_Wage(){
+        companyEmpWageArray = new CompanyEmpWage[5];
+    }
+
+    public void addCompany(String company,int EMP_RATE_PER_HOUR,int EMP_WORK_DAYS,int EMP_WORK_HOUR){
+
+        companyEmpWageArray[noOfCompany] = new CompanyEmpWage(company, EMP_RATE_PER_HOUR, EMP_WORK_DAYS, EMP_WORK_HOUR);
+        noOfCompany++;
+    }
+
+    public void computeEmpWage(){
+        for(int i=0;i<noOfCompany;i++){
+            companyEmpWageArray[i].setTotalWage(this.calculateEmployeeWage(companyEmpWageArray[i]));
+            System.out.println(companyEmpWageArray[i]);
+        }
+    }
+
+
+    public int calculateEmployeeWage(CompanyEmpWage companyEmpWage){
 
         int emp_wage = 0;
         int total_emp_work_hour = 0;
         int total_emp_work_day = 0;
 
-        while (total_emp_work_hour <= EMP_WORK_HOUR && total_emp_work_day < EMP_WORK_DAYS) {
+        while(total_emp_work_hour<=companyEmpWage.EMP_WORK_HOUR && total_emp_work_day<companyEmpWage.EMP_WORK_DAYS){
 
-            total_emp_work_day++;
-            int emp_check = (int) Math.floor(Math.random() * 10 % 3);
+            total_emp_work_day ++;
+            int emp_check = (int)Math.floor(Math.random()*10%3);
 
-            if (emp_check == FULL_TIME_EMP) {
-                emp_wage = EMP_RATE_PER_HOUR * EMP_FULLTIME_WORK_HOUR;
+            if(emp_check == FULL_TIME_EMP){
+                emp_wage = companyEmpWage.EMP_RATE_PER_HOUR * EMP_FULLTIME_WORK_HOUR;
                 total_emp_work_hour += EMP_FULLTIME_WORK_HOUR;
-            } else if (emp_check == PART_TIME_EMP) {
-                emp_wage = EMP_RATE_PER_HOUR * EMP_PARTTIME_WORK_HOUR;
+            }
+            else if(emp_check == PART_TIME_EMP){
+                emp_wage = companyEmpWage.EMP_RATE_PER_HOUR * EMP_PARTTIME_WORK_HOUR;
                 total_emp_work_hour += EMP_PARTTIME_WORK_HOUR;
-            } else {
+            }
+            else{
                 emp_wage = 0;
                 total_emp_work_hour += 0;
             }
-            total_wage += emp_wage;
+            companyEmpWage.total_wage += emp_wage;
         }
-        return total_wage;
+        return companyEmpWage.total_wage;
     }
 
-    //@Override
-    public String toString() {
-        return "Total Employee Wage for company " + company + " is : " + total_wage;
-    }
+
 
     public static void main(String[] args) {
 
-        Employee_Wage emp1 = new Employee_Wage("Nokia", 400, 25, 150);
-        emp1.calculateEmployeeWage();
-        System.out.println(emp1);
+        Employee_Wage empwage = new Employee_Wage();
+        empwage.addCompany("Samsung", 20, 25, 200);
+        empwage.addCompany("QualityKiosk", 25, 24, 300);
+        empwage.addCompany("Bridgelabz", 30, 25, 250);
+        empwage.computeEmpWage();
+        // empwage.calculateEmployeeWage(null);
 
-        Employee_Wage emp2 = new Employee_Wage("Apple", 600, 27, 162);
-        emp2.calculateEmployeeWage();
-        System.out.println(emp2);
+
     }
 }
